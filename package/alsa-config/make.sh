075b13337d1e2514fa3bce8d877c360589811d11
Caesar Wang <wxt@rock-chips.com>
2020-09-27 17:18:14 +0800

    package: update alsa-config
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I7432157d02d2138e2219bf2aa9744d59be1eb081

--- package/alsa-config/make.sh
+++ package/alsa-config/make.sh
@@ -3,4 +3,4 @@
 set -e
 DEPENDENCIES=alsa-utils
 $SCRIPTS_DIR/build_pkgs.sh $ARCH $SUITE $DEPENDENCIES
-install -m 0644 -D $TOP_DIR/external/alsa-config/cards/* $TARGET_DIR/usr/share/alsa/cards/
+install -m 0644 -D $TOP_DIR/external/alsa-config/alsa/cards/* $TARGET_DIR/usr/share/alsa/cards/
